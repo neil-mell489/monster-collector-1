da5bfe8b13c10e90f26ed29ee8fe27ccb2e921bb
neil-mell489 <neilmellstrom97@gmail.com>
2024-05-13 06:52:34 -0700

    working index page

--- monstercollector_app/models.py
+++ monstercollector_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Monster(models.Model):
+    name = models.CharField(max_length=100) 
+    level = models.IntegerField(max_length=100)  
+    attribute = models.CharField(max_length=100) 
+    type = models.CharField(max_length=100)  
+    lore = models.TextField(max_length=400) 
+
+
+    def __str__(self):
+        return self.name
--- monstercollector_app/views.py
+++ monstercollector_app/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Monster
+
+
+monsters = [
+    {
+     "name": "Blue-Eyes White Dragon", 
+     "level": "8", 
+     "attribute": "Light", 
+     "type": "Dragon", 
+     "lore": "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.", 
+},
+
+    {
+     "name": "Dark Magician", 
+     "level": "7", 
+     "attribute": "Dark", 
+     "type": "Spellcaster", 
+     "lore": "The ultimate wizard in terms of attack and defense.", 
+},
+
+    {
+     "name": "Kuriboh", 
+     "level": "1", 
+     "attribute": "Dark", 
+     "type": "Fiend/Effect", 
+     "lore": "During damage calculation, if your opponent's monster attacks (Quick Effect): You can discard this card; you take no battle damage from that battle.", 
+},
+]
+
 
 # Create your views here.
 def home(request):
@@ -8,3 +37,13 @@ def home(request):
 
 def about(request):
     return render(request, 'monsters/about.html')
+
+def index(request):
+    monsters = Monster.objects.all()
+    return render(request, 'monsters/index.html', {
+        'monsters': monsters
+        })
+
+def monster_detail_view(request, pk):
+    monster = Monster.objects.get(pk=pk)
+    return render(request, 'monsters/detail.html', {'monster': monster})
