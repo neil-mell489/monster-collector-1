93c90f94fc957821fd3846b7f9dc2552f074732a
neil-mell489 <neilmellstrom97@gmail.com>
2024-05-22 10:26:35 -0700

    version 1.0 done

--- monstercollector_app/views.py
+++ monstercollector_app/views.py
@@ -10,6 +10,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
+
+
 # Create your views here.
 def home(request):
     return render(request, 'monsters/home.html')
